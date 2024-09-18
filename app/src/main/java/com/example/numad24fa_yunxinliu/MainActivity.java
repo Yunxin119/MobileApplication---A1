bc4ac6ace3de61894e421835fa5a63f71d8fc03c
Yunxin Liu <1999yunxin@gmail.com>
2024-09-18 13:35:40 -0700

    Hello World Application

---
+++ app/src/main/java/com/example/numad24fa_yunxinliu/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.numad24fa_yunxinliu;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button aboutMe = findViewById(R.id.about_me_button);
+        aboutMe.setOnClickListener(view -> {
+            String text = "Name: Yunxin Liu \n Email: liu.yunx@northeastern.edu";
+            Toast.makeText(MainActivity.this, text, Toast.LENGTH_LONG).show();
+        });
+
+    }
+}
