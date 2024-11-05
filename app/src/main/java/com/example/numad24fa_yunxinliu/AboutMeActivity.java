b926f3920255516159cabb84689b151199cb2328
Yunxin Liu <1999yunxin@gmail.com>
2024-11-05 14:28:22 -0800

    about me activity and new button to navigate

--- app/src/main/java/com/example/numad24fa_yunxinliu/AboutMeActivity.java
+++ app/src/main/java/com/example/numad24fa_yunxinliu/AboutMeActivity.java
@@ -1,9 +1,6 @@
 package com.example.numad24fa_yunxinliu;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,33 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button contacts_collector;
+public class AboutMeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_about_me);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        Button aboutMe = findViewById(R.id.about_me_button);
-        aboutMe.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, AboutMeActivity.class);
-            startActivity(intent);
-        });
-
-        contacts_collector = findViewById(R.id.contacts_collector);
-        contacts_collector.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, ContactsCollectActivity.class);
-            startActivity(intent);
-        });
-
-
     }
 }
